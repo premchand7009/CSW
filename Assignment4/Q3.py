ee309ca16c9ecd2b9e7090fe11589870bc5fd75d
Prem Chand Patra <premchandpatra2007@gmail.com>
2025-12-04 23:10:59 +0530

    update

--- Assignment4/Q3.py
+++ Assignment4/Q3.py
@@ -1,5 +1,6 @@
 import re
-s="Contact: +91-9876543210, Office: (91) 98765 43211, Home: 0091 9876543212"
-format=r"(\+\d{2}\-)\d{10}|(\(\d{2}\)\s)\d{5}\s\d{5}|\d{4}\s\d{10}"
+s = "Contact: +91-9876543210, Office: (91) 98765 43211, Home: 0091 9876543212  91 9876543210"
 
-print(re.sub(format,"+91-XXXXXXXXXX",s))
+pattern = r"(?:\+\d{2}-\d{10})|(?:\(\d{2}\)\s\d{5}\s\d{5})|(?:0091\s\d{10})|(?:\d{2}\s\d{10})"
+
+print(re.sub(pattern, "+91-XXXXXXXXXX", s))
